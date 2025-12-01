e1d26b2dec956923a7bd83cf14280df963a16aed
carlosbue3 <141794660+carlosbue3@users.noreply.github.com>
2025-12-01 15:06:18 -0500

    depuracion

--- temp_backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-cd /d "C:\xampp\htdocs\Backend-2.0\backend" 
-set "JAVA_HOME=C:\Program Files\Java\jdk-21" 
-set "PATH=%JAVA_HOME%\bin;%PATH%" 
-echo Java configurado: %JAVA_HOME% 
-echo Iniciando Spring Boot... 
-mvn spring-boot:run 
-pause 
